d8027acfe46286273192e2186ec6a2f2525e079c
aleksandra-rodzinka <118135484+aleksandra-rodzinka@users.noreply.github.com>
2022-11-24 00:54:14 +0100

    Linked elements to JavaScript
    Now everything is updated from SheCodes API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionsElement = document.querySelector("#conditions");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  conditionsElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let ApiKey = "f0c81td68bc7aeb6ae5b7113o45714af";
+let city = "Dubai";
+let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric`;
+
+console.log(ApiUrl);
+
+axios.get(ApiUrl).then(displayTemperature);
